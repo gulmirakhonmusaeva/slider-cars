ab87a5d7636acb17c48df6b4b20d43bd3b68fcfd
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2022-02-10 23:53:06 +0500

    add owl slider

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
             sliddesWrapper = document.querySelector('.offer__slider-wrapper'),
             width = window.getComputedStyle(sliddesWrapper).width,
             slidesField = document.querySelector('.offer__slider-inner'),
+            slider = document.querySelector('.offer__slider'),
             // dark mode
-            darkMode = document.querySelector('.dark-btn');
+            darkMode = document.querySelector('.dark-btn'),
             lightMode = document.querySelector('.light-btn');
 
 
@@ -49,8 +50,52 @@ window.addEventListener('DOMContentLoaded', () => {
                 slide.style.width = width;
             } )
 
+
+        //////// 10
+        slider.style.position = 'relative';
+         let  dots = []
+        let indicator = document.createElement('ol');
+        indicator.style.cssText =`
+            position: absolute;
+            right: 0;
+            bottom: 40px;
+            left: 0;
+            z-index: 15;
+            display:flex;
+            justify-content:center;
+            margin-left:15%;
+            margin-right:15%;
+            list-style:none;
+        `
+        slider.append(indicator)
+
+        for (let i = 0 ; i < slides.length; i++){
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.style.cssText = `
+                box-sizing:content-box;
+                flex: 0 1 auto;
+                width: 40px;
+                height: 6px;
+                margin-left: 3px;
+                margin-right: 3px;
+                cursor:pointer;
+                background-color: #fff;
+                background-clip: padding-box;
+                border-top: 10px solid transparent;
+                border-bottom: 10px solid transparent;
+                opacity: .5;
+                transition: opacity .6s ease;
+            `
+            if (i == 0){
+            dot.style.opacity = 1;
+            }
+            indicator.append(dot)
+            dots.push(dot);
+        }
+
             next.addEventListener('click', () => {
-                if (offset == (width.slice(0,width.length - 2) * (slides.length - 1 ))){
+                if (offset == (width.slice(0, width.length - 2) * (slides.length - 1 ))){
                     offset = 0
                     console.log(offset);
                 } else {
@@ -72,7 +117,29 @@ window.addEventListener('DOMContentLoaded', () => {
                     current.textContent = slideIndex;
                 }
 
+                dots.forEach(dot => dot.style.opacity = '.4')
+                dots[slideIndex - 1].style.opacity = 1 ;
 
+            })
+
+            ///// 11 
+
+            dots.forEach(dot => {
+                dot.addEventListener('click',e => {
+                    const slideTo = e.target.getAttribute('data-slide-to');
+                    slideIndex = slideTo;
+                    offset = (+width.slice(0 , width.length - 2) * (slideTo - 1))
+
+                    slidesField.style.transform = `translateX (-${offset}px)`
+
+                    if (slides.length < 10) {
+                        current.textContent = `0${slideIndex}`;
+                    } else {
+                        current.textContent = slideIndex;
+                    }
+                    dots.forEach(dot => dot.style.opacity = '.4')
+                    dots[slideIndex - 1].style.opacity = 1 ;
+                })
             })
 
 
